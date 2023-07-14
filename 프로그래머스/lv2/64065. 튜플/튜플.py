0f6a1228aeeaa507094528f4748bdff822240aec
ksw0733 <102013032+ksw0733@users.noreply.github.com>
2023-07-14 20:53:00 +0900

    [level 2] Title: 튜플, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -0,0 +1,10 @@
+import re
+from collections import Counter
+
+def solution(s):
+    
+    s = Counter(re.findall('\d+', s))
+
+    answer = list(map(int, [k for k, v in sorted(s.items(), key = lambda x: x[1], reverse = True)]))
+    
+    return answer
