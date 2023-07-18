fc5016fc411c3ff863772133a1bd15b5631705f3
ksw0733 <102013032+ksw0733@users.noreply.github.com>
2023-07-18 21:14:47 +0900

    [level 2] Title: 타겟 넘버, Time: 4.49 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,21 @@
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    n = len(numbers)
+    queue.append([numbers[0], 0])
+    queue.append([-1 * numbers[0], 0])
+    
+    while queue:
+        tmp, idx = queue.popleft()
+        idx += 1
+        
+        if idx < n:
+            queue.append([tmp+numbers[idx], idx])
+            queue.append([tmp-numbers[idx], idx])
+            
+        else:
+            if tmp == target:
+                answer += 1
+                
+    return answer
