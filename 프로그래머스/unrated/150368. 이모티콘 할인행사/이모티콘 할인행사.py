2fac6c35bec40c006c3f2e80b6cafb07330745b0
ksw0733 <102013032+ksw0733@users.noreply.github.com>
2023-09-21 20:39:25 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -0,0 +1,26 @@
+from itertools import product
+
+
+def solution(users, emoticons):
+    answer = []
+    sales = [10, 20, 30, 40]
+
+    for case in product(sales, repeat=len(emoticons)):
+        result = [0, 0]
+        
+        for user in users:
+            temp = 0
+            for idx, sale in enumerate(case):
+                if sale >= user[0]:
+                    temp += emoticons[idx] * (100 - sale) // 100
+
+            if temp >= user[1]:
+                result[0] += 1
+            else:
+                result[1] += temp
+
+        answer.append(result)
+
+    answer.sort(key=lambda x: (-x[0], -x[1]))
+
+    return answer[0]
