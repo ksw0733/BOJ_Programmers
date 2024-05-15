911c5c9bee1adc0c9b0cb722672bdd00454556d0
ksw0733 <102013032+ksw0733@users.noreply.github.com>
2024-05-15 16:42:49 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+def solution(n):
+    sugar = [-1] * 5001
+    sugar[3] = sugar[5] = 1
+
+    for i in range(6, n + 1):
+        if i % 5 == 0:
+            sugar[i] = sugar[i - 5] + 1
+
+        elif i % 3 == 0:
+            sugar[i] = sugar[i - 3] + 1
+
+        elif sugar[i - 3] > 0 and sugar[i - 5] > 0:
+            sugar[i] = min(sugar[i - 3], sugar[i - 5]) + 1
+            
+    return sugar[n]
+
+
+if __name__ == "__main__":
+    n = int(input())
+    print(solution(n))
