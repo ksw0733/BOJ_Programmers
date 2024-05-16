59c354e2f15564499b470eea7a284d7847ea99f8
ksw0733 <102013032+ksw0733@users.noreply.github.com>
2024-05-16 20:26:09 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 184 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def solution(n):
+    for i in range(n):
+        dp[i] = card_pack[i]
+        for j in range(i):
+            dp[i] = min(dp[i], dp[i - j - 1] + card_pack[j])
+            
+    return dp[-1]
+        
+if __name__ == "__main__":
+    n = int(input())
+    card_pack = list(map(int, input().split()))
+    dp = [0] * n
+    print(solution(n))
