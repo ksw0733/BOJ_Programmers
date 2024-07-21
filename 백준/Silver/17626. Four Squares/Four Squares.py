6857ed623d9a77be934533d407fa2a2f91edf8b9
ksw0733 <102013032+ksw0733@users.noreply.github.com>
2024-07-21 18:43:08 +0900

    [Silver III] Title: Four Squares, Time: 52 ms, Memory: 31552 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -2,22 +2,21 @@ import sys
 input = sys.stdin.readline
 
 def solution(n):
-    sq = [0 if i**0.5%1 else 1 for i in range(n+1)]
+    sq = [0 if i**0.5 % 1 else 1 for i in range(n+1)]
     
-    mincount = 4
+    if sq[n]:
+        return 1
+
     for i in range(int(n**0.5), 0, -1):
-        if sq[n] :
-            mincount=1
-            break
-        elif sq[n-i**2] :
-            mincount=2
-            break
-        else:
-            for j in range(int((n-i**2)**0.5), 0, -1):
-                if sq[(n-i**2)-j**2]:
-                    mincount=3
-                    
-    return mincount
+        if sq[n - i**2]:
+            return 2
+
+    for i in range(int(n**0.5), 0, -1):
+        for j in range(int((n - i**2)**0.5), 0, -1):
+            if sq[n - i**2 - j**2]:
+                return 3
+
+    return 4
 
 if __name__ == '__main__':
     n = int(input())
